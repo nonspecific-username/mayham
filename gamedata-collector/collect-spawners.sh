a0314af8d183df6b46aec67c084035b9b79ccf48
user name <user@email>
2021-04-05 15:32:20 +0400

    Make jwp serialization optional for map files

--- gamedata-collector/collect-spawners.sh
+++ gamedata-collector/collect-spawners.sh
@@ -3,6 +3,7 @@
 
 GAMEDATA_PATH=${GAMEDATA_PATH}
 OUTPUT_FILE=${OUTPUT_FILE}
+JWP_SERIALIZE=${JWP_SERIALIZE:-no}
 JWP_CMD=${JWP_CMD:-"wine64 /home/dn/projects/bl3/jwp/apo-jwp.exe"}
 WINDEBUG=fixme-all
 
@@ -13,13 +14,19 @@ echo $GAMEDATA_PATH > $OUTPUT_FILE
 for MAPFILE in $(find $GAMEDATA_PATH -name '*.umap'); do
   echo "Checking map file $MAPFILE..."
   if strings $MAPFILE | grep -qE 'Spawner'; then
-    echo "Found spawner data in $MAPFILE, attempting to serialize"
-    $JWP_CMD serialize $(echo $MAPFILE | sed 's/\.umap//g') 2>&1 > /dev/null
+    echo "Found spawner data in $MAPFILE"
     SERIALIZED_FILE=$(echo $MAPFILE | sed 's/\.umap/.json/g')
-    if [ ! -e "$SERIALIZED_FILE" ]; then
-        echo "WARNING: jwp could not serialize map file $MAPFILE"
+    if [[ "$JWP_SERIALIZE" == 'yes' ]]; then
+      $JWP_CMD serialize $(echo $MAPFILE | sed 's/\.umap//g') 2>&1 > /dev/null
+      if [ ! -e "$SERIALIZED_FILE" ]; then
+          echo "WARNING: jwp could not serialize map file $MAPFILE"
+      else
+          echo "$SERIALIZED_FILE" | tee -a $OUTPUT_FILE
+      fi
     else
-        echo "$SERIALIZED_FILE" | tee -a $OUTPUT_FILE
+      if [[ -e $SERIALIZED_FILE ]]; then
+          echo "$SERIALIZED_FILE" | tee -a $OUTPUT_FILE
+      fi
     fi
   else
     echo "No spawner data in $MAPFILE"
