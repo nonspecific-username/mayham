050fee2c94bd33ce044f72e274b2300d0bfa0778
user name <user@email>
2021-06-17 01:28:03 +0400

    Hide implementation details for persistent store

--- persistent/persistent.go
+++ persistent/persistent.go
@@ -10,7 +10,13 @@ import (
 )
 
 
-func WatchFile(path string) (*dsl.MultiDSLConfig, chan bool, chan bool, error, *[]error) {
+var (
+    syncCh chan bool
+    stopCh chan bool
+)
+
+
+func Open(path string) (*dsl.MultiDSLConfig, error, *[]error) {
     var cfg dsl.MultiDSLConfig
     var errs *[]error
     if _, err := os.Stat(path); err != nil && os.IsNotExist(err) {
@@ -18,26 +24,26 @@ func WatchFile(path string) (*dsl.MultiDSLConfig, chan bool, chan bool, error, *
     } else {
         data, err := ioutil.ReadFile(path)
         if err != nil {
-            return nil, nil, nil, err, nil
+            return nil, err, nil
         }
 
         cfg, err, errs = dsl.LoadMulti(&data)
         if err != nil {
-            return nil, nil, nil, err, errs
+            return nil, err, errs
         }
     }
 
-    needToSync := make(chan bool)
-    shouldStop := make(chan bool)
+    syncCh = make(chan bool)
+    stopCh = make(chan bool)
 
     ticker := time.NewTicker(1 * time.Second)
     go func () {
         for {
             select {
-            case <-shouldStop:
+            case <- stopCh:
                 ticker.Stop()
                 return
-            case <- needToSync:
+            case <- syncCh:
                 newData := cfg.String()
                 ioutil.WriteFile(path, []byte(newData), 0644)
             case <-ticker.C:
@@ -45,5 +51,17 @@ func WatchFile(path string) (*dsl.MultiDSLConfig, chan bool, chan bool, error, *
         }
     }()
 
-    return &cfg, needToSync, shouldStop, nil, nil
+    return &cfg, nil, nil
+}
+
+
+func Close() {
+    Sync()
+    time.Sleep(time.Second)
+    stopCh <- true
+}
+
+
+func Sync() {
+    syncCh <- true
 }
