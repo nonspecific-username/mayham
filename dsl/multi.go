139af03594aca5444073aa21892bb0269cfff605
user name <user@email>
2021-06-16 20:11:22 +0400

    Added support for multiple mods in one file

---
+++ dsl/multi.go
@@ -0,0 +1,46 @@
+package dsl
+
+
+import (
+    "errors"
+    "fmt"
+
+    yaml "gopkg.in/yaml.v2"
+)
+
+
+type MultiDSLConfig map[string]*DSLConfig
+
+
+func LoadMulti(input *[]byte) (MultiDSLConfig, error, *[]error) {
+    var errorsOutput []error
+    cfg := make(map[string]*DSLConfig)
+    err := yaml.UnmarshalStrict(*input, cfg)
+
+    if err != nil {
+        return nil, err, nil
+    }
+
+    for id, subCfg := range cfg {
+        errs := subCfg.Validate()
+        if len(*errs) > 0 {
+            for _, e := range(*errs) {
+                msg := fmt.Sprintf("DSLConfig[%s]: %v", id, e)
+                errorsOutput = append(errorsOutput, errors.New(msg))
+            }
+
+        }
+    }
+    if len(errorsOutput) > 0 {
+        return nil, errors.New("Failed to validate MultiDSLConfig"), &errorsOutput
+    }
+
+    return cfg, nil, nil
+}
+
+
+func (cfg MultiDSLConfig) String() string {
+    str, _ := yaml.Marshal(cfg)
+
+    return string(str)
+}
