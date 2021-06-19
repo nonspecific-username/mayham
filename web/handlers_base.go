9cc76d9f65cbbb0adb1f299c167c6a5729b4d564
user name <user@email>
2021-06-20 00:00:32 +0400

    Implemented GET/DELETE for mods

--- web/handlers_base.go
+++ web/handlers_base.go
@@ -3,13 +3,28 @@ package web
 
 import (
     "log"
+    "fmt"
+
+    "github.com/nonspecific-username/mayham/web/state"
 
     "github.com/gin-gonic/gin"
 )
 
 
+const (
+    templateMod404 string = "Error: mod with id: %s doesn't exist"
+)
+
+
 func handleGetModList(c *gin.Context) {
     log.Printf("handleGetModList")
+
+    mods := make([]string, 0, len(*runtimeCfg))
+    for mod := range *runtimeCfg {
+        mods = append(mods, mod)
+    }
+
+    c.JSON(200, &mods)
 }
 
 
@@ -20,6 +35,14 @@ func handleCreateMod(c *gin.Context) {
 
 func handleGetMod(c *gin.Context) {
     log.Printf("handleGetMod")
+
+    key := c.Param("mod")
+
+    if mod, ok := (*runtimeCfg)[key]; ok {
+        c.JSON(200, mod)
+    } else {
+        c.Data(404, gin.MIMEHTML, []byte(fmt.Sprintf(templateMod404, key)))
+    }
 }
 
 
@@ -30,4 +53,15 @@ func handleUpdateMod(c *gin.Context) {
 
 func handleDeleteMod(c *gin.Context) {
     log.Printf("handleDeleteMod")
+
+    key := c.Param("mod")
+
+    if _, ok := (*runtimeCfg)[key]; ok {
+        delete(*runtimeCfg, key)
+        state.Sync()
+        c.Data(204, gin.MIMEHTML, nil)
+    } else {
+        c.Data(404, gin.MIMEHTML, []byte(fmt.Sprintf(templateMod404, key)))
+    }
+
 }
